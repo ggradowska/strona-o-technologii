21e3b68bd85f1a12ddcc0cd675b7ae35fbd4bd1d
Gabriela 3 <156536282+ggradowska@users.noreply.github.com>
2024-06-05 21:02:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDetails(techId) {
+    var details = document.getElementById("details-" + techId);
+    if (details.style.display === "none" || details.style.display === "") {
+      details.style.display = "block";
+    } else {
+      details.style.display = "none";
+    }
+  }
+  
